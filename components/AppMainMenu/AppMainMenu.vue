1e05c4178a922a15a1a641b0357b7d250c645563
alban <albanvincent.pro@gmail.com>
2024-03-17 16:46:35 +0100

    save

--- components/AppMainMenu/AppMainMenu.vue
+++ components/AppMainMenu/AppMainMenu.vue
@@ -5,6 +5,13 @@
 </template>
 <style lang="scss" scoped>
 #sidenav {
-    
+    width: 250px;
+    height: 95dvh;
+    position: fixed;
+    top: 50%;
+    transform: translate(0, -50%);
+    background-color: $secondary-color;
+    box-shadow: 0px 0px 18px 0px $tertiary-color;
+    border-radius: 1.5rem;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['@/assets/style/main.scss'],
+  css: ['@/assets/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/scss/variables/variables.scss";',
+        },
+      },
+    },
+  },
 })
