e37da4777dff2ea7dc0f48d4e1f6e9cbb40d5d73
alban <albanvincent.pro@gmail.com>
2024-03-21 15:02:48 +0100

    save

--- components/AppMainMenu/AppMainMenu.vue
+++ components/AppMainMenu/AppMainMenu.vue
@@ -1,64 +1,103 @@
 <template>
     <div class="sidenav-container">
-        <nav id="sidenav" :class="{ 'closed': !menuOpeningState }">
+        <nav id="sidenav">
             <ToggleButton 
                 @click="toggleMenuOpeningState"
                 :closed="menuOpeningState"
             />
             <div id="sidenav__body" :class="{'closed': !menuOpeningState}">
-
+                <div id="sidenav__body__background" :class="{ 'closed': !menuOpeningState }"></div >
+                <div id="sidenav__body__wrapper" :class="{ 'closed': !menuOpeningState }" >
+                    <h1>{{ pageTitle }}</h1>
+                    
+                </div>
             </div>
             <div id="sidenav__footer" :class="{'closed': !menuOpeningState}">
-
-            </div>  
+                <div id="sidenav__footer__background" :class="{ 'closed': !menuOpeningState }"></div>
+              
+            </div>
         </nav>
     </div>
 </template>
 <script lang="ts" setup>
 import ToggleButton from './ToggleButton.vue';
 
-const menuOpeningState = ref(false);
+const menuOpeningState: Ref<boolean> = ref(false);
+const pageTitle: string = "Alban Vincent";
 
 const toggleMenuOpeningState = () => {
     menuOpeningState.value = !menuOpeningState.value;
 };
+
+onMounted(() => {
+    setTimeout(() => {
+        menuOpeningState.value = true;
+    }, 1000);
+});
 </script>
 <style lang="scss" scoped>
 .sidenav-container {
     position: relative;
 
     #sidenav {
+
         &__body {
-            position: fixed;
-            top: -20dvh;
-            left: -220px;
-            width: 400px;
-            height: 150dvh;
-            background-color: $secondary-color;
-            transform: rotate(12deg);
-            box-shadow: 0px 0px 18px 0px $tertiary-color;
-            transition: ease-in-out 0.4s;
+            position: relative;
+            width: 250px;
+            height: 100dvh;
+            filter: drop-shadow(0 0 0.75rem $tertiary-color);
+            transition: all 1s ease-in-out;
+            left: 0;
 
             &.closed {
-                left: -150dvw;
-                top: -50dvh;
+                left: -200dvw;
             }
+
+            &__background {
+                position: absolute;
+                left: -100%;
+                top: -50%;
+                height: 150vh;
+                width: 550px;
+                background-color: $secondary-color;
+                clip-path: polygon(0 0, 100% 25%, 50% 100%, 0 100%);
+            }
+
+            &__wrapper {
+                width: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                top: 10px;
+                color: white;
+            }
+
+            
         }
 
         &__footer {
-            position: fixed;
-            bottom: -324px;
-            left: -222px;
-            width: 400px;
+            position: relative;
+            width: 300px;
             height: 100dvh;
-            background-color: $secondary-color;
-            transform: rotate(-26deg);
-            box-shadow: 0px 0px 18px 0px $tertiary-color;
-            transition: ease-in-out 0.7s;
-
-            &.closed {
+            filter: drop-shadow(0 0 0.75rem $tertiary-color);
+            
+            &__background {
+                position: absolute;
                 left: -100%;
-                bottom: -50%;
+                top: -50%;
+                height: 150vh;
+                width: 550px;
+                background-color: $secondary-color;
+                clip-path: polygon(0 0, 100% 25%, 50% 100%, 0 100%);
+                transition: all 0.3s ease-in-out;
+                z-index: 1;
+                transition: all 0.5s ease-in-out;
+
+                &.closed {
+                    left: -200dvw;
+                }
             }
         }
     }
